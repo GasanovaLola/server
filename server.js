5851816796881b4e5a80d2ddcb53e1c6448076e7
Lola Gasanova <63504067+GasanovaLola@users.noreply.github.com>
2021-09-26 17:26:48 +0300

    commit second

--- server.js
+++ server.js
@@ -1,42 +1,15 @@
-const http = require('http');
+const http = require("http");
 
-const html = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="app.css">
-    <title>Document</title>
-</head>
-<body>
-    <h1> rest server </h1>
-</body>
-<script src="app.js"></script>
-</html>`;   
+const hostname = "127.0.0.1";
+const port = 8000;
 
-const css = `body {
-    margin: 25px;
-}`;
-const js = `alert('correct')`;
+const server = http.createServer((req, res) => {
 
-http.createServer((req, res) => {
-    switch (req.url) {
-        case '/':
-            res.writeHead(200, { 'Content-Type': 'text/html'});
-            res.end(html);
-            break;
-        case '/app.css':
-            res.writeHead(200, { 'Content-Type': 'text/css'});
-            res.end(css);
-            break;
-        case '/app.js':
-            res.writeHead(200, { 'Content-Type': 'text/javascript'});
-            res.end(js);
-            break;
-        default:
-            res.writeHead(404, { 'Content-Type': 'text/plain'});
-            res.end('404');
-            break;
-    }
-}).listen(3000, () => console.log('Сервер работает'));
+   res.writeHead(200, {'Content-Type': 'text/plain'});
+
+   res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+   console.log(`Server running at http://${hostname}:${port}/`);
+})
